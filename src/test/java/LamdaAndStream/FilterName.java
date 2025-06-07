d89bde1d2ec6bbaa2d56c0b2492da91923ac5ed4
Raghul <129582758+RAGHUL-RAJAN@users.noreply.github.com>
2025-06-07 21:56:58 +0530

    Update FilterName.java

--- src/test/java/LamdaAndStream/FilterName.java
+++ src/test/java/LamdaAndStream/FilterName.java
@@ -7,11 +7,8 @@ import java.util.stream.Collectors;
 public class FilterName {
 
     public static void main(String[] args) {
-
         List<String> names = Arrays.asList("Alice", "Jack", "Andrew", "David");
-
         List<String> Filtername = names.stream().filter( name -> name.startsWith("A")).collect(Collectors.toList());
-
         System.out.println(Filtername);
     }
 }
